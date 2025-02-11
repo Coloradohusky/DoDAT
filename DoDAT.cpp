4e0e7d0d907b3f952cf3a9dd970ff95f059ff940
schellingb <14200249+schellingb@users.noreply.github.com>
2025-02-12 04:49:58 +0900

    Increment version to 0.7

--- DoDAT.cpp
+++ DoDAT.cpp
@@ -3187,7 +3187,7 @@ int main(int argc, char *argv[])
 			"  -f         : Fix meta data in existing files that fail verification\n"
 			"  -c         : Use fast CRC only check for verification and fixing\n"
 			"  -q         : Don't ask for pressing a key at the end\n"
-			"\n", "DoDAT", "0.6");
+			"\n", "DoDAT", "0.7");
 		return 1;
 	}
 
