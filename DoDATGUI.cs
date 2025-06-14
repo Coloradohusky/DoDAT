3a18e8c5fd2c135f4e93d7376e5a9e216913f6af
schellingb <14200249+schellingb@users.noreply.github.com>
2025-06-14 23:33:23 +0900

    Increment version to 0.8

--- DoDATGUI.cs
+++ DoDATGUI.cs
@@ -24,13 +24,13 @@ using System.Xml;
 
 [assembly: System.Reflection.AssemblyTitle("DoDATGUI")]
 [assembly: System.Reflection.AssemblyProduct("DoDATGUI")]
-[assembly: System.Reflection.AssemblyVersion("0.7.0.0")]
-[assembly: System.Reflection.AssemblyFileVersion("0.7.0.0")]
+[assembly: System.Reflection.AssemblyVersion("0.8.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("0.8.0.0")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 
 class DoDATGUI : Form
 {
-    const string DoDatExeVersionString = "DoDAT v0.7";
+    const string DoDatExeVersionString = "DoDAT v0.8";
 
     string OriginalText;
 
